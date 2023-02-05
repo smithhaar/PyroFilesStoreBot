85f39bc857b8eddaa2c46c3eba7ea4af713e15dc
smithhaar <124481464+smithhaar@users.noreply.github.com>
2023-02-05 11:31:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,25 +4,25 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "0")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	API_ID = int(os.environ.get("API_ID", "26364421"))
+	API_HASH = os.environ.get("API_HASH", '72c7598f883fa1b077358d6c86071654')
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", '6193747791:AAH-f1uYxy8VLUDf_ujWw-YD0sMXFjGkldg')
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "filestore5")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001742058974"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1823620269"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://random:random@cluster0.tb63h.mongodb.net/cluster0?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001742838735")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001742058974")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", False))
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
-🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
+🤖 **My Name:** [Files Store Bot](https://t.me/filestore5_bot)
 
 📝 **Language:** [Python3](https://www.python.org)
 
